50f5f903d12a38c8531824f63e1cddf76b5e5b82
Khasiano <NewbieCoder10>
2019-10-11 11:52:25 -0500

    Initial commit for Padovan.

--- src/edu/cnm/deepdive/Padovan.java
+++ src/edu/cnm/deepdive/Padovan.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive;
 
+import java.util.Scanner;
+
 /**
  * Implements a generator for the Padovan sequence. This generator takes the form of a static
  * method returning the first <em>n</em> terms of the sequence as a {@code long[]}.
@@ -23,6 +25,40 @@ public class Padovan {
    * @return first {@code n} terms.
    */
   public static long[] generate(int n) {
+
+
+    class PadovanMain{
+
+      public void main(String[] args) {
+
+        int firstNumber = 1;
+        int secondNumber = 1;
+        int thirdNumber = 1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of terms for Padovan sequence : ");
+
+        int noOfTerms = scanner.nextInt();
+        int nextNumber;
+
+        System.out.print( firstNumber + " " + secondNumber + " " + thirdNumber);
+
+        for(int i = 1; i <= noOfTerms - 3; i++) {
+
+          nextNumber = secondNumber + firstNumber;
+          System.out.print(" " + nextNumber);
+          firstNumber = secondNumber;
+          secondNumber = thirdNumber;
+          thirdNumber = nextNumber;
+
+        }
+
+
+      }
+
+    }
+
     // TODO Compute and return the first n terms, where p0 = 0, p1 = 1, p2 = 1, etc.
     return null;
   }
---
+++ src/edu/cnm/deepdive/PadovanTest.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PadovanTest {
+
+  @org.junit.jupiter.api.Test
+  void generate() {
+  }
+
+  @Test
+  void testGenerate() {
+  }
+}
